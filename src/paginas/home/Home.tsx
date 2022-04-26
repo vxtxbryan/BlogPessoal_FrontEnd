fe31c551052d77b051eada42ed2e1800982bd563
Victor Oliveira Da Silva <100298866+vxtxbryan@users.noreply.github.com>
2022-04-26 18:44:58 -0300

    Front-End React Blog Pessoal

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+        <img src="https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-post-de-blog_114360-154.jpg?w=2000" alt="Imagem Home" className="img"/>
+        </>
+    );
+}
+
+export default Home;
